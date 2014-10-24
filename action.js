76239381b11c2f4912d61640b438411b3cbeec64
pavankumarlpersonal <pavankumarlpersonal@gmail.com>
2014-10-24 22:57:31 +0530

    bower

--- action.js
+++ action.js
@@ -1,12 +1,12 @@
     var action = (function (){
-    
+
         var actionjs = function(){
-    
+
           var VERSION = "1.0.0";
           var hash = window.location.hash;
-            
-            
-            
+
+
+
           /*
             action List heirarchy
             'actionName' : {
@@ -21,62 +21,62 @@
                                     ],
                             'priorities' : [1,2,3]
                          }
-            
+
           */
           var actionsList = [];
           var routerList = [];
-    
+
           var uiActions = 'click dblclick mousedown mouseup mouseover mouseout mousemove mouseenter mouseleave keydown keyup keypress submit focus blur copy cut paste';
-    
+
           var forEach = function(loopElements , functionRef){
               var loop = loopElements.split(' ');
               for(var i=0; i<loop.length; i++){
                   functionRef(loop[i]);
               }
           };
-    
+
           var compile = function(elem){
-    
+
                 elem = elem ? elem : document;
-    
+
                 forEach(uiActions , function(action){
-    
+
                     var actionElements = elem.querySelectorAll('[action-' + action + ']');
-    
+
                     if(!actionElements)
                         return;
-                    
+
                     for(var i=0; i<actionElements.length; i++){
                         var actionElem = actionElements[i];
-    
+
                         actionElem.addEventListener(action , function (event){
                             triggerAction(event.target.getAttribute('action-'+action) , event);
                         });
                     }
-    
+
                 });
           };
-    
+
         var defaultAction = {
             async_wait : false,
             priority : 5,
             action : function(){},
             dependencies : ''
         };
-         
+
         var extend = function(defaultObj , actualObj){
-            
+
             for(var key in defaultObj){
                 var value = actualObj[key];
-                
+
                 if(!value)
                     actualObj[key] = defaultObj[key];
             }
-            
+
             return actualObj;
         }
-             
-             
+
+
         /*
             action skeleton
               {
@@ -85,210 +85,218 @@
                   'priority' : priority of the action, priority 1 will be executed first, no limit on highest bound
                   'functionname' : functionRef
               }
-    
+
           */
-            
+
           var addAction = function(actionName , action){
-              
+
               action = extend(defaultAction , action);
-              
+
               var actionSkeleton = actionsList[actionName];
-    
+
               if(!actionSkeleton){
-                
+
                 actionSkeleton = {};
-                  
+
                 actionSkeleton.actions = {};
                 actionSkeleton.priorities = [];
-                  
+
                 actionsList[actionName] = actionSkeleton;
-                  
+
               }
-              
+
               if(actionSkeleton.priorities.indexOf(action.priority) === -1){
                   actionSkeleton.priorities.push(action.priority);
-                  
+
                   actionSkeleton.actions[action.priority] = [];
-                  
+
                   // sort as per priority
                   actionSkeleton.priorities.sort();
               }
-              
+
               var prioritizedAction = actionSkeleton.actions[action.priority];
-              
+
               var actionInfo = {};
               actionInfo.async_wait = action.async_wait;
               actionInfo.action = action.action;
               actionInfo.dependencies = action.dependencies;
-              
+
               prioritizedAction.push(actionInfo);
-              
-              
+
+
           };
-    
+
           var Scope = function(actionName){
                 this.name = actionName;
           };
-            
+
           Scope.prototype.set = function(key, value){
               if(key)
                 this[key] = value;
           };
-          
+
           Scope.prototype.get = function(key){
-                return this[key];   
+                return this[key];
           };
-            
-          // state 
-            
+
+          // state
+
           var ASYNC_DONE = 1;
           var ASYNC_ERROR = -1;
-            
+
           var AsyncExecution = function(callback){
                 this.callback = callback;
           };
-          
+
           AsyncExecution.prototype.resolve = function(){
                 this.callback.call(ASYNC_DONE);
           };
-            
+
           AsyncExecution.prototype.error = function(){
                 this.callback.call(ASYNC_ERROR);
           };
-            
+
           var triggerAction = function(actionName , arguments){
               // trigger action with arguments
-              
+
               var action = actionsList[actionName];
-              
+
               if(!action)
                   return;
-              
+
               var actionArguments = [];
-              
+
               if(arguments){
                    actionArguments.push(arguments);
               }
-              
+
               actionArguments.push(new Scope(actionName));
-              
+
               executePriority(action , action.priorities , actionArguments , 0);
-              
+
           };
-            
+
           var executePriority = function(actionRef, priorities , actionArguments , position){
-            
+
               if(position >= priorities.length){
-                return;   
+                return;
               }
-              
+
                var priority = priorities[position];
-                      
+
                var prioritizedActions = actionRef.actions[priority];
-              
-               var onDone = function(){
-                   executePriority(actionRef , priorities , actionArguments , position+1);
-               };
-              
-               var priority = new PriorityInvoker(prioritizedActions , actionArguments , onDone);
-              
-              
-              
+
+              //  var onDone = function(){
+               //
+              //  };
+
+               var priority = new PriorityInvoker(prioritizedActions , actionArguments );
+
+               priority.onDone(function(){
+                  executePriority(actionRef , priorities , actionArguments , position+1);
+               });
+
                priority.onTerminate(function(){
                 // do nothing
                });
-              
+
                priority.executeActions();
-              
+
                //executeAction(actionsOfPriority , scope)
-              
+
           };
-            
-          var PriorityInvoker = function(prioritizedActions ,actionArguments , done){
+
+          var PriorityInvoker = function(prioritizedActions ,actionArguments ){
               this.prioritizedActions = prioritizedActions;
               this.actionArguments = actionArguments;
-              this.done = done;
-              
+
               this.asyncWaitQueue = 0;
-              
+
               this.asyncWaitCallback = function(asyncStatus){
-                  this.asyncWaitQueue = this.asyncWaitQueue - 1;  
+                  this.asyncWaitQueue = this.asyncWaitQueue - 1;
                   if(asyncStatus === ASYNC_DONE){
                             if(this.asyncWaitQueue == 0){
-                                PriorityInvoker.onDone.call();       
+                                this.done();
                             }
-                        
+
                     }else if(asyncStatus === ASYNC_ERROR){
-                           PriorityInvoker.onTerminate.call();
+                           this.terminate();
                     }
               };
-              
-              
-              this.onDone = function(){
-                  this.onDone.call();
+
+              this.done = function(){
+                  this.onDoneFunctionRef.call(null);
               };
-                
-              this.onTerminate = function(callback){
-                    //callback.call();  
+
+              this.terminate = function(){
+                  this.onTerminateFunctionRef.call(null);
               };
-              
+
           };
-            
+
+          PriorityInvoker.prototype.onDone = function(functionRef){
+              this.onDoneFunctionRef = functionRef;
+          };
+
+          PriorityInvoker.prototype.onTerminate = function(functionRef){
+              this.onTerminateFunctionRef = functionRef;
+          };
+
           PriorityInvoker.prototype.executeActions = function(){
               for(var i=0; i<this.prioritizedActions.length; i++){
                    var ac = this.prioritizedActions[i];
-                   
+
                    if(ac.async_wait === true){
                        var asyncExecution = new AsyncExecution(this.asyncWaitCallback);
                        this.asyncWaitQueue = this.asyncWaitQueue + 1;
-                       
+
                        this.actionArguments.push(asyncExecution);
                    }
-                  
+
                   if(!ac.action.apply(null , this.actionArguments)){
-                        this.onTerminate.call(this);
+                        this.terminate();
                   }
                }
-              
+
               if(this.asyncWaitQueue === 0){
-                 this.done.call();
+                 this.done();
               }
           };
-            
-         
-            
+
+
+
           var addURLAction = function(actionsJSON){
-    
+
               for(action in actionsJSON){
                     routerList[action] = actionsJSON[action];
                   //addAction(action , actionData);
               }
-    
+
           };
-    
+
           // poll for hash change
-          
-          setInterval(function(){
-              
-              var presentHash = window.location.hash;
-              
-              if(hash != presentHash){
-                 hash = presentHash;
-                 triggerHashChange(); 
-              }
-              
-          },500);
-            
+
+          // setInterval(function(){
+          //
+          //     var presentHash = window.location.hash;
+          //
+          //     if(hash != presentHash){
+          //        hash = presentHash;
+          //        triggerHashChange();
+          //     }
+          //
+          // },500);
+
           var triggerHashChange = function(){
               var presentHash = hash.substr(1,hash.length);
               if(routerList[presentHash]){
                 triggerAction(routerList[presentHash]);
                 compile();
               }
-              
-              
+
+
           };
-            
+
           var navigate = function(hash , data){
               if(routerList[hash]){
                 window.location.hash = hash;
@@ -296,7 +304,7 @@
                   compile();
               }
           };
-            
+
           window.onload = function(){
               if(hash){
                 var presentHash = hash.substr(1,hash.length);
@@ -306,41 +314,41 @@
               }
               compile();
           };
-    
+
           return {
-    
+
               router : function (actionsJSON){
                   addURLAction(actionsJSON);
               },
-    
+
               on : function(actionData , action){
                   addAction(actionData , action);
               },
-    
+
               version : function (){
                   return VERSION;
               },
-              
+
               trigger : function(actionName , arguments){
                   triggerAction(actionName , arguments);
               },
-              
+
               navigate : function(hash , data){
-                navigate(hash , data);   
+                navigate(hash , data);
               }
-    
+
           }
-    
+
         };
-        
+
          var previousAction = actionjs();
-    
+
           var action = previousAction;
-    
+
           action.noConflict = function(){
               return previousAction;
           }
-    
+
           return action;
-    
+
     })();
--- example/try.js
+++ example/try.js
@@ -14,8 +14,9 @@ action.on('toHome' , {
 });
 
 action.on('toContact' , {
-    action : function(){
-        document.getElementById('hash').innerHTML = "Contact" 
+    action : function(data){
+        console.log(data);
+        document.getElementById('hash').innerHTML = "Contact"
         return true;
     }
 });
@@ -24,8 +25,8 @@ action.on('malla' , {
     priority:2,
     action : function(scope){
         console.log(scope.get('name'));
-        
-        document.getElementById('hash').innerHTML = "malla" 
+
+        document.getElementById('hash').innerHTML = "malla"
         return true;
     }
 });
@@ -35,7 +36,12 @@ action.on('malla' , {
     action : function(scope){
         scope.set('name' , 'pavan');
         console.log(1);
-        document.getElementById('hash').innerHTML = "malla" 
-        return true;
+        document.getElementById('hash').innerHTML = "malla"
+
+        setTimeout(function(){
+          action.navigate('contact' , 'abc');
+        },1000);
+
+        return false;
     }
 });
